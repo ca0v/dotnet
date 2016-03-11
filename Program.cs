9ac60da153358b481a9cf2a4df2343031c49062a
coreyalix <coreyalix@hotmail.com>
2016-03-10 21:02:33 -0500

    dotnet lab

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello {args[0]}!");
+            string[] files = Directory.GetFiles("./", "*.json", SearchOption.AllDirectories);
+            foreach (var file in files) {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
